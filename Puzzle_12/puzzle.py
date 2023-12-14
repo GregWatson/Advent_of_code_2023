a6d9f2419b73795706192498f0bca0367a389ca9
Greg Watson <gwatson@gmail.com>
2023-12-13 16:38:36 -0800

    Puzz12 still working on part 2

--- Puzzle_12/puzzle.py
+++ Puzzle_12/puzzle.py
@@ -26,12 +26,8 @@ def get_reports(db):
         data = l.split()
         s = '?'.join([data[0],data[0],data[0],data[0],data[0]])
         n = ','.join([data[1],data[1],data[1],data[1],data[1]])
-        nums=n.split(',')
-        num=0
-        for n in nums:
-            num=100*num + int(n)
-        # print(f"{data[1]} {num}")
-        reps.append((s, num))
+        nums = map(int, n.split(','))
+        reps.append((s, list(nums)))
     return reps
 
 # Return sequence ID for s. String is only . or #
@@ -92,6 +88,79 @@ def get_combs(r):
 
     return valids
 
+
+def get_valid_strings(s: str,nums: list ,in_hash=False, lev=0) -> list:
+    pad = '   '*lev
+    # print(f"{pad}--- str:{s}  nums:{nums}  in:{in_hash}")
+    len_s = len(s)
+    # print(f"l {len_s}", end="\r")
+    len_nums = len(nums)
+    min_str_len = sum(nums) + len_nums - 1
+    if len_s < min_str_len: return 0
+    if len_s == 0 and len_nums == 0: return [ '' ]
+    if len_s == 0 and len_nums == 1 and nums[0] == 0: return [ '' ]
+    if len_s == 0 and len_nums > 0: return [] # fail
+    # if no numbers left then there cannot be any hashes left.
+    if len_nums == 0:
+        if '#' in s: return [] # fail
+        # else make sure no ? left
+        new_string = s.replace("?", ".")
+        return [new_string]  # only possible
+    if len_s == 1:
+        if (s =='#' and len_nums==1 and nums[0]==1) : 
+            # print(f"{pad}Good: return [{s}]") 
+            return [s]  # good
+        if (s =='?' and len_nums==1 and nums[0]==1) : return ['#']  # good
+        if (s =='?' and len_nums==0 ) : return ['.']  # good
+        if (s =='.' and len_nums==0 ) : return ['.']  # good
+    # string of at least 2 chars, still looking for at least one #
+    c = s[0]
+    n = nums[0]
+    # print(f"{pad}c={c}  n={n}")
+    if n==0 and not in_hash:
+        print(f"Error. if n is 0 then must be in_hash")
+        sys.ext(1)
+    if in_hash :
+        if n > 0: 
+            if c in '#?':
+                nums[0] = n-1
+                sL = get_valid_strings(s[1:], nums, in_hash, lev=lev+1)
+                # print(f"{pad}recursion returned {sL}")
+                if len(sL) : 
+                    return [ '#' + ss for ss in sL ] 
+            return [] # fail
+        if n == 0 and c in '.?' : 
+            sL = get_valid_strings(s[1:], nums[1:], in_hash=False, lev=lev+1)
+            # print(f"{pad}recursion returned {sL}")
+            return [ '.' + ss for ss in sL ]
+        else: return [] # fail
+    # Not in hash
+    if c == '.':
+        sL = get_valid_strings(s[1:], nums, in_hash, lev=lev+1)
+        # print(f"{pad}recursion returned {sL}")
+        if len(sL) : 
+            return [ '.' + ss for ss in sL ]
+        else : return []
+    if c == '#':
+        nums[0] = n-1
+        sL = get_valid_strings(s[1:], nums, in_hash=True, lev=lev+1)
+        if len(sL) : 
+            return [ '#' + ss for ss in sL ]
+        return [] # fail
+    # c is '?' - try both
+    # first assume its a '.'
+    s_dot = get_valid_strings(s[1:], nums[:], in_hash, lev=lev+1)
+    s1 = [ '.' + ss for ss in s_dot ]
+    nums[0] = n-1
+    s_hash = get_valid_strings(s[1:], nums, in_hash=True, lev=lev+1)
+    # print(f"{pad}recursion returned {s_hash}")
+    s2 = [ '#' + ss for ss in s_hash ]
+    # print(f"{pad}s1 is {s1}  s2 is {s2}   ")
+    s1.extend(s2)
+    return s1
+
+
+
 #---------------------------------------------------------------------------------------
 # Load input
 db = load_db()
@@ -104,10 +173,10 @@ reps = get_reports(db) # list of tuple ( orig_string, seq_ID)
 
 tot = 0
 for r in reps:
-    # r = ('?.???????..?##???', 603)
-    print(f"Process{r}")
-    combs = get_combs(r)
-    print(f"Valids for {r} are {combs}")
-    tot = tot + len(combs)
+    # r = ( '????..????.?????.????', [1,1,1,1])
+    print(f"Working on {r}")
+    sL = get_valid_strings(r[0], r[1])
+    print(f" -- {len(sL)} valids for {r}")
+    if sL: tot = tot + len(sL)
     # break
 print(f"tot is {tot}")
